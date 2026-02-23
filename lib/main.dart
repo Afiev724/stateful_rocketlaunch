643b0a93a4fbcb573207024bc532b4377cdcc592
Farhiya <fafrah003@gmail.com>
2026-02-23 13:35:13 -0500

    Initital commit first two tasks

--- lib/main.dart
+++ lib/main.dart
@@ -9,54 +9,82 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Rocket Launch Controller',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: CounterWidget(),
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const CounterWidget(),
     );
   }
 }
 
 class CounterWidget extends StatefulWidget {
+  const CounterWidget({super.key});
+
   @override
   _CounterWidgetState createState() => _CounterWidgetState();
 }
 
 class _CounterWidgetState extends State<CounterWidget> {
-  // set counter value
   int _counter = 0;
+
+  Color getColor() {
+    if (_counter == 0) return Colors.red;
+    if (_counter <= 50) return Colors.orange;
+    return Colors.green;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Rocket Launch Controller'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: Container(
+      appBar: AppBar(title: const Text('Rocket Launch Controller')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
               color: Colors.blue,
+              padding: const EdgeInsets.all(20),
               child: Text(
-                '$_counter',
-                style: const TextStyle(fontSize: 50.0),
+                _counter == 100 ? 'LIFTOFF!' : '$_counter',
+                style: TextStyle(fontSize: 50, color: getColor()),
               ),
             ),
-          ),
-          Slider(
-            min: 0,
-            max: 100,
-            value: _counter.toDouble(),
-            onChanged: (double value) {
-              setState(() {
-                _counter = value.toInt();
-              });
-            },
-            activeColor: Colors.blue,
-            inactiveColor: Colors.red,
-          ),
-        ],
+            Slider(
+              min: 0,
+              max: 100,
+              value: _counter.toDouble(),
+              onChanged: (double value) {
+                setState(() {
+                  _counter = value.toInt();
+                });
+              },
+              activeColor: Colors.blue,
+              inactiveColor: Colors.red,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  if (_counter < 100) _counter++;
+                });
+              },
+              child: const Text('Ignite'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  if (_counter > 0) _counter--;
+                });
+              },
+              child: const Text('Decrement'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  _counter = 0;
+                });
+              },
+              child: const Text('Reset'),
+            ),
+          ],
+        ),
       ),
     );
   }
